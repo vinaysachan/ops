64d0cb4ac104285191673485b475eaca6bc5d5cd
Vinay Sachan <vinaysachan@live.in>
2014-11-25 00:27:58 +0530

    refs #Site Header Added

--- index.php
+++ index.php
@@ -1,1 +1,146 @@
-Vinay Sachan
+<!DOCTYPE html>
+<html lang="en">
+    <head>  
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Page Title</title>
+        <meta name="description" content="Page Desc">
+        <meta name="author" content="Vinay Sachan"/> 
+        <meta name="Keywords" content="Page Desc">
+        <meta name="copyright" content="&copy; 2014 Online PHP Study" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="shortcut icon" href="logo.png"/>
+        <link href="public/bootstrap-3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+        <link href="public/font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" media="screen"/>
+        <link href="public/style/opsV1.css" rel="stylesheet" media="screen"/> 
+    </head>
+    <body>
+        <header class="">
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <div class="col-md-4 col-sm-3">
+                        <a title="" href="#">
+                            <img class="img-responsive logo_img" src="public/img/new_logo.png">
+                        </a>
+                        <span class="navbar-header pull-right">
+                            <button class="navbar-toggle pull-right collapsed" data-target="#navbar-collapse-top" data-toggle="collapse" type="button">
+                                <i class="fa fa-bars"></i>
+                                Top Menu
+                            </button>
+                        </span>
+                    </div>
+                    <div class="col-md-8 col-sm-9">
+                        <ul id="social-header">
+                            <li>
+                                <a class="twitter" href="#">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li> 
+                            <li>
+                                <a class="gplus" href="#">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="fb" href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="linkedin" href="#">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="youtube" href="#">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="rss" href="#">
+                                    <i class="fa fa-rss"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div> 
+                        <div id="search_box">
+                            <form class="navbar-right col-lg-6"> 
+                                <div class="row input-group">
+                                    <input type="text" class="form-control"placeholder="Search here ..."  >
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-primary" type="button">Search</button>
+                                    </span>
+                                </div><!-- /input-group --> 
+                            </form><!-- /.col-lg-6 -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+                <div class=" ">
+                    <div class="collapse navbar-collapse" id="navbar-collapse-top">
+                        <ul class="nav navbar-nav"> 
+                            <li class="active dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul> 
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Link</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+            </div><!-- /.container-fluid -->
+        </nav>
+
+
+        <script src="public/script/jquery-2.1.1.min.js"></script>
+        <script src="public/bootstrap-3.2.0/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
